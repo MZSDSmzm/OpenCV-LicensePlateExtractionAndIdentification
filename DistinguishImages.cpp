25396cf00958d27c5ae34b05c1da9a0d6c5d3ae7
MZSDSmzm <31851461+MZSDSmzm@users.noreply.github.com>
2018-02-26 18:06:14 +0800

    提取图像

--- DistinguishImages.cpp
+++ DistinguishImages.cpp
@@ -15,12 +15,7 @@ int main()
 	if (!src.data) {
 		return 0;
 	}
-	//高斯模糊GaussianBlur /均值模糊blur
-	//第一个参数是读入的图片，dst是输出图片，也是Mat类型，
-	//Size(3,3)是矩阵内核，必须是奇数，数值越大越模糊。
-	//GaussianBlur(src, bilateralFilterSrc, Size(3, 3),0);
-	//blur(src, bilateralFilterSrc, Size(3, 3));
-	//imshow("blur", src_gray);
+	
 	Mat bilateralFilterSrc;
 	//双边滤波处理
 	bilateralFilter(src, bilateralFilterSrc, 25, 25 * 2, 25 / 2);
@@ -29,6 +24,8 @@ int main()
 	//转化为灰度图 
 	cvtColor(bilateralFilterSrc, src_gray, CV_BGR2GRAY);
 	imshow("src_gray", src_gray);
+	
+	
 	waitKey();
 	return 0;
 }
